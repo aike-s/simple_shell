dd09f12834559f7d6dddba2822031588b12e3d78
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-04-13 17:58:45 -0500

    [<fix ~ get_error.c](<print execution errors>)

---
+++ get_error.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+/**
+ * get_error - Function that prints errors
+ *
+ * @count: Variable that counts the commands entered
+ * @line_argument: Line of argument recalled
+ */
+void get_error(size_t count, char *line_argument)
+{
+	int length = 0;
+
+	length = _strlen(line_argument);
+	if (line_argument[0] == '\n') /*si es un enter*/
+	{
+		write(STDERR_FILENO, "", 1);
+	}
+	else if (length < 256)
+	{
+		write(STDERR_FILENO, "/bin/hsh: ", 11);
+		print_number(count);
+		write(STDERR_FILENO, ": ", 2);
+		write(STDERR_FILENO, line_argument, length);
+		write(STDERR_FILENO, ": not found\n", 13);
+	}
+	else
+	{
+		write(STDERR_FILENO, "/bin/hsh: ", 11);
+		print_number(count);
+		write(STDERR_FILENO, ": ", 2);
+		write(STDERR_FILENO, line_argument, length);
+		write(STDERR_FILENO, ": File name too long\n", 22);
+	}
+}
