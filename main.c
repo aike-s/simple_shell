7cfaac75eed592055cb2a5f4a6b3c1a58212593f
aike-s <sagrilo804@gmail.com>
2021-04-13 18:38:11 -0500

    [add] file main.c with main utilities

---
+++ main.c
@@ -0,0 +1,63 @@
+#include "shell.h"
+/**
+ * main - main donde llamamos a todas las funciones necesarías
+ *
+ * @arc: arc void
+ * @arv: arv void
+ * @env: enviromment donde traemos todos los formatos necesarios
+ * Return: Always 0
+ */
+int main(int arc, char **arv, char **env)
+{
+	(void)arc;
+	(void)arv;
+
+	char *line = NULL, **tokens, path[BUFFER_SIZE];
+	garbage_collector_t *GC;
+	dir_t *head;
+	size_t bufsize = 0, count = 1;
+	ssize_t char_read;
+
+	GC = create_garbage_collector();
+	copy_path_from_env(env, path);
+	head = dir_list(GC, path);
+	signal(SIGINT, handle_sigint); /* <-- ctrl + c */
+
+	while (1)
+	{
+		write(1, "$ ", 2);
+		char_read = getline(&line, &bufsize, stdin);
+
+		if (char_read == -1) /* EOF -> Ctrl+d */
+		{
+			GC->free_all(GC);
+			free_garbage_collector(GC);
+			free(GC);
+			free(line);
+			_putchar('\n');
+			return (0);
+		}
+		if (char_read > 1)
+			line[char_read - 1] = '\0';
+
+		tokens = _split(GC, line);
+
+		if (compare_str(tokens[0], "exit"))
+		{
+			GC->free_all(GC);
+			free_garbage_collector(GC);
+			free(GC);
+			free(line);
+			return (0);
+		}
+
+		tokens[0] = register_token(GC, tokens[0], head, count++);
+		/*if the command does not exist restart the loop*/
+		if (tokens[0] == NULL)
+			continue;
+
+		exec(GC, tokens, env, line);
+	}
+
+	return (0);
+}
